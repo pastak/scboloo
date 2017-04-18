27200b276f676db8fed09a58898d559afc631f40
pastak <pasta0915@gmail.com>
2017-04-18 16:49:44 +0900

    improve development environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const WebpackOnBuildPlugin = require('on-build-webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const exec = require('child_process').execSync
+
+const isProductionBuild = process.env.NODE_ENV === 'production'
 
 let plugins = [
   new CopyWebpackPlugin([
@@ -12,21 +15,38 @@ let plugins = [
   new WebpackOnBuildPlugin(() => {
     exec('cp -R dist/common/* dist/chrome')
     exec('cp -R dist/common/* dist/firefox')
+    exec('cp -R dist/common/* dist/msedge')
     exec('./node_modules/.bin/wemf -U --browser firefox dist/firefox/manifest.json')
     exec('./node_modules/.bin/wemf -U --browser chrome dist/chrome/manifest.json')
+    exec('./node_modules/.bin/wemf -U --browser edge dist/msedge/manifest.json')
   })
 ]
 
+if (isProductionBuild) plugins.push(new UglifyJSPlugin())
+
 module.exports = {
+  devtool: isProductionBuild ? 'inline-source-map' : false,
   entry: {
-    main: ['chrome-browser-object-polyfill', './src/main.js'],
+    main: ['chrome-browser-object-polyfill', 'babel-polyfill', './src/main.js'],
     content: ['chrome-browser-object-polyfill', 'babel-polyfill', './src/content.js'],
-    option: ['chrome-browser-object-polyfill', './src/option/option.js'],
-    popup: ['chrome-browser-object-polyfill', './src/popup/popup.js']
+    option: ['chrome-browser-object-polyfill', 'babel-polyfill', './src/option/option.js'],
+    popup: ['chrome-browser-object-polyfill', 'babel-polyfill', './src/popup/popup.js']
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist/common')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: [['es2015', { 'modules': false }], 'es2017']
+        }
+      }
+    ]
   },
   plugins
 }
